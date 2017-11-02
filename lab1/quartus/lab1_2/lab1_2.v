54245eae042637deb929573e4952f315642e0275
Vadim Dyachkov <vadik.dyachkov@gmail.com>
2017-11-02 15:40:31 +0300

    init lab1

---
+++ lab1/quartus/lab1_2/lab1_2.v
@@ -0,0 +1,12 @@
+// В описании можно использовать операторы Bitwise, Logical, Reduction, Replicator, Concatenate.
+module lab1_2(sw, led, pba);
+input [7:0] sw;
+input pba;
+output [3:0] led;
+
+wire [3:0] pba_vec;
+assign pba_vec = {4 {pba}};
+
+assign led = (sw[3:0] & ~pba_vec) | (sw[7:4] & pba_vec);
+
+endmodule
